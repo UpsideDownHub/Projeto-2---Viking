2f0faa723106c4c5711017c42212c087c2a6649b
Lucas Francisco de Araujo <lucas1999araujo@gmail.com>
2018-02-02 19:16:35 -0200

    Arquivo correto
    desconsiderar a versão anterior

--- Assets/Script/PlayerMoviment.cs
+++ Assets/Script/PlayerMoviment.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
 {
-    public Vector2 mousePosition;
 
-    [SerializeField] float speedWalk = 5;
-    [SerializeField] float speedRun = 4;
-    [SerializeField] Transform shotPrefab;
+    [SerializeField] float speedWalk = 4;
+    [SerializeField] float speedRun = 5;
     float speed;
     Vector2 move;
     
     Rigidbody2D rb = new Rigidbody2D();
-
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    
     void Update()
     {
-        #region PlayerMoviment
+
         if (Input.GetKey(KeyCode.LeftShift))
             speed = speedRun;
         else
@@ -39,32 +37,19 @@ public class PlayerMoviment : MonoBehaviour
             Vector3 moveLand = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime);
             transform.position += moveLand * speed;
         }
-        #endregion
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(shotPrefab, transform.position,shotPrefab.rotation);
-            //ShotMoviment sm = GetComponent<ShotMoviment>();
-            //sm.CallShoot(mousePosition);
-        }
     }
 
-    //#region FlourRegion
-    //private void OnTriggerStay2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.layer == LayerMask.NameToLayer("Ice"))
-    //    {
-    //        speedRun = 4;
-    //        speedWalk = 3;
-    //    }
-    //    else
-    //    {
-    //        speedRun = 5;
-    //        speedWalk = 4;
-    //    }
-    //}
-    //#endregion
-
-
-
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ice"))
+        {
+            speedRun = 4;
+            speedWalk = 3;
+        }
+        else
+        {
+            speedRun = 5;
+            speedWalk = 4;
+        }
+    }
 }
