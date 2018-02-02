8ea63aa5f473276ced0675c33442008dbed5ed9d
Lucas Francisco de Araujo <lucas1999araujo@gmail.com>
2018-02-02 20:36:56 -0200

    Mecanica de tiro

--- Assets/Script/PlayerMoviment.cs
+++ Assets/Script/PlayerMoviment.cs
@@ -7,19 +7,20 @@ public class PlayerMoviment : MonoBehaviour
 
     [SerializeField] float speedWalk = 4;
     [SerializeField] float speedRun = 5;
+    [SerializeField] Transform shot;
     float speed;
     Vector2 move;
-    
+
     Rigidbody2D rb = new Rigidbody2D();
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    
+
     void Update()
     {
-
+        #region Player Moviment
         if (Input.GetKey(KeyCode.LeftShift))
             speed = speedRun;
         else
@@ -37,8 +38,18 @@ public class PlayerMoviment : MonoBehaviour
             Vector3 moveLand = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime);
             transform.position += moveLand * speed;
         }
+        #endregion
     }
 
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(shot, transform.position, shot.rotation);
+        }
+    }
+
+    #region Land Collision
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ice"))
@@ -52,4 +63,6 @@ public class PlayerMoviment : MonoBehaviour
             speedWalk = 4;
         }
     }
+    #endregion
+    
 }
---
+++ Assets/Script/ShotMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMovement : MonoBehaviour {
+
+    [SerializeField] float speed = 5;
+    Vector3 mouse;
+
+	void Start () {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = -1;
+	}
+	
+	void FixedUpdate () {
+        int T;
+
+        transform.position = Vector3.MoveTowards(transform.position, mouse, speed * Time.deltaTime);
+
+        if(transform.position == mouse)
+        {
+            T = 0;
+        }
+        else
+        {
+            T = 5;
+        }
+
+        Destroy(gameObject, T);
+
+    }
+}
