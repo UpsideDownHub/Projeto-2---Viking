9ddb0975e13d84bba433a311bc1ea6cdcf7b0661
Lucas Francisco de Araujo <lucas1999araujo@gmail.com>
2018-02-03 18:16:17 -0200

    Controle da camera

---
+++ Assets/Script/MoveCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+    
+    private Vector3 offset;         
+    
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && Camera.main.orthographicSize < 9 ) // forward
+        {
+            Camera.main.orthographicSize++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f && Camera.main.orthographicSize > 5 ) // backwards
+        {
+            Camera.main.orthographicSize--;
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
