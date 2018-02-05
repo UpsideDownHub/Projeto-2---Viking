4858e8a3360054c311642e1570d455b5daf2415c
Lucas Francisco de Araujo <lucas1999araujo@gmail.com>
2018-02-05 19:51:53 -0200

    Melhoria nos tiros

--- Assets/Script/EnemyShotMovement.cs
+++ Assets/Script/EnemyShotMovement.cs
@@ -2,31 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyShotMovement : MonoBehaviour {
+public class EnemyShotMovement : MonoBehaviour
+{
 
     [SerializeField] float speed = 5;
     Vector3 PlayerPosition;
     [SerializeField] Transform Player;
+    Vector3 direction;
 
     private void Start()
     {
         Player = GameObject.Find("Player").transform;
-        PlayerPosition = new Vector3(Player.transform.position.x, Player.transform.position.y,0);
+
+        PlayerPosition = new Vector3(Player.transform.position.x, Player.transform.position.y, 0);
+
+        direction = PlayerPosition - transform.position;
     }
 
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, PlayerPosition, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        Destroy(gameObject, 3);
+    }
 
-        if (transform.position == PlayerPosition)
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             Destroy(gameObject);
         }
-        else
-        {
-            Destroy(gameObject, 5);
-        }
-
 
     }
 }
--- Assets/Script/PlayerShotMovement.cs
+++ Assets/Script/PlayerShotMovement.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 
 public class PlayerShotMovement : MonoBehaviour
 {
-
     [SerializeField] float speed = 5;
     Vector3 mouse;
+    Vector3 direction;
 
     void Start()
     {
         mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = -1;
+        direction = mouse - transform.position;
     }
 
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, mouse, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
 
-        if (transform.position == mouse)
+        Destroy(gameObject, 3);
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             Destroy(gameObject);
         }
-        else
-        {
-            Destroy(gameObject, 5);
-        }
-
 
     }
 }
