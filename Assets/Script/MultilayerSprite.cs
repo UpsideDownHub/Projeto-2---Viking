51088a9a8711865ca8aab780f5ee6d04b4996aa0
cleberaraujolima <cleberaraujolima@users.noreply.github.com>
2018-02-16 20:54:17 -0200

    CHECKPOINT

---
+++ Assets/Script/MultilayerSprite.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MultilayerSprite : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float offset;
+    SpriteRenderer rend, targetRend;
+
+	void Start () {
+        rend = GetComponent<SpriteRenderer>();
+        targetRend = target.GetComponent<SpriteRenderer>();
+	}
+	
+	void Update () {
+        if (target.position.y >= transform.position.y + offset)
+            rend.sortingOrder = targetRend.sortingOrder + 1;
+        else
+            rend.sortingOrder = targetRend.sortingOrder - 1;
+    }
+}
