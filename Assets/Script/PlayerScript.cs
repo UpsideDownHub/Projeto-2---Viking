1114f0d3aa9ab3326563c8f2e596fae4f9011c5c
Lucas Francisco de Araujo <lucas1999araujo@gmail.com>
2018-03-03 16:42:04 -0300

    Player Animation Demo

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,20 +11,24 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] Transform playerFoot;
     [SerializeField] int playerLife;
     [SerializeField] GameObject Item;
+    [SerializeField] Animator playerAnimator;
     float shootRate = 1f;
     float shootCoolDown;
     string kinfOfFlour;
-    
+
     public int typeOfShot = 1;
     float speed;
     Vector2 move;
     Vector3 target;
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
     void Start()
     {
         shootCoolDown = 0;
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -35,6 +40,18 @@ public class PlayerScript : MonoBehaviour
         }
         #endregion
 
+
+        playerAnimator.SetFloat("speedX", Input.GetAxis("Horizontal"));
+        playerAnimator.SetFloat("speedY", Input.GetAxis("Vertical"));
+
+        if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") < 0 || Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = true;
+        }
+        else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") < 0 || Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+        {
+            sr.flipX = false;
+        }
     }
 
     private void FixedUpdate()
@@ -51,10 +68,50 @@ public class PlayerScript : MonoBehaviour
             move.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             move.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
             rb.velocity += move;
+
         }
         else
         {
             rb.velocity = new Vector2(0, 0);
+
+            float lastInputX = Input.GetAxis("Horizontal");
+            float lastInputY = Input.GetAxis("Vertical");
+
+            if (lastInputX != 0 || lastInputY != 0)
+            {
+                playerAnimator.SetBool("walking", true);
+                if (lastInputX > 0)
+                {
+                    playerAnimator.SetFloat("lastMoveX", 1f);
+                }else if(lastInputX < 0)
+                {
+                    playerAnimator.SetFloat("lastMoveX", -1f);
+                }
+                else
+                {
+                    playerAnimator.SetFloat("lastMoveX", 0f);
+                }
+
+                if(lastInputY > 0)
+                {
+                    playerAnimator.SetFloat("lastMoveY", 1f);
+                }else if(lastInputY < 0)
+                {
+                    playerAnimator.SetFloat("lastMoveY", 1f);
+                }
+                else
+                {
+                    playerAnimator.SetFloat("lastMoveY", 0f);
+                }
+            }
+            else
+            {
+                playerAnimator.SetBool("walking", false);
+            }
+
+
+            
+
             Vector3 moveLand = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime);
             transform.position += moveLand * speed;
         }
@@ -62,11 +119,11 @@ public class PlayerScript : MonoBehaviour
 
         #region Turn Player
 
-        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        target.z = 0f;
+        //target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //target.z = 0f;
 
-        var angle = Mathf.Atan2(target.y - transform.position.y, target.x - transform.position.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        //var angle = Mathf.Atan2(target.y - transform.position.y, target.x - transform.position.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0, 0, angle);
 
         #endregion
 
